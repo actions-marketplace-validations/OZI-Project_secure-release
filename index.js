5b52da41d5e6ce83b249a4c4550961d728c45cad
Eden Ross Duff, MSc, DDiv <rjdbcm@outlook.com>
2025-05-11 19:54:45 -0500

    allow sdist build as well
    Signed-off-by: Eden Ross Duff, MSc, DDiv <rjdbcm@outlook.com>

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { exec } = require('@actions/exec');
 
+core.setSecret(core.getInput('wheel-sign-token'));
+
+var args = [
+  '-m',
+  'invoke',
+  '--search-root=.tox/invoke/tmp/subprojects/ozi',
+  'release',
+  core.getBooleanInput('sdist') ? ['--sdist'] : [],
+  `--wheel-sign-token=${core.getInput('wheel-sign-token')}`
+]
+
 try {
-  await exec('python', ['-m', 'invoke', '--search-root=.tox/invoke/tmp/subprojects/ozi', 'release', `--wheel-sign-token=${core.getInput('wheel-sign-token')}`]);
+  await exec('python', args.flat());
 } catch (error) {
   core.setFailed(error.message);
 }
