91fcdc7fc5d32d01ab4068ac4f87b540c583519b
Eden Ross Duff, MSc, DDiv <rjdbcm@outlook.com>
2025-05-12 02:14:14 -0500

    fix async logic
    Signed-off-by: Eden Ross Duff, MSc, DDiv <rjdbcm@outlook.com>

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const { exec } = require('@actions/exec');
 
 core.setSecret(core.getInput('wheel-sign-token'));
 
-var args = [
-  '-m',
-  'invoke',
-  '--search-root=.tox/invoke/tmp/subprojects/ozi',
-  'release',
-  core.getBooleanInput('sdist') ? ['--sdist'] : [],
-  `--wheel-sign-token=${core.getInput('wheel-sign-token')}`
-];
-
-try {
-  await exec('python', args.flat());
-} catch (error) {
-  core.setFailed(error.message);
+const build = async function(){
+  var args = [
+    '-m',
+    'invoke',
+    '--search-root=.tox/invoke/tmp/subprojects/ozi',
+    'release',
+    core.getBooleanInput('sdist') ? ['--sdist'] : [],
+    `--wheel-sign-token=${core.getInput('wheel-sign-token')}`
+  ];
+  
+  try {
+    await exec('python', args.flat());
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+build()
